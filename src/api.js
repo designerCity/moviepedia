b247feb0e78d8d4ac53ee8c70127af4503b4d6a0
designerCity <97098829+designerCity@users.noreply.github.com>
2022-03-13 20:27:25 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,9 +3,13 @@
 export async function getReviews({
   order = 'createdAt', offset = 0, limit = 6,    
 }) {
+  // throw new Error('버그가 아니라 기능입니다!')
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const response = await fetch(`https://learn.codeit.kr/api/film-reviews?${query}`)
+  if (!response.ok) {
+    throw new Error('리뷰를 불러오는데 실패했습니다.')
+  }
   const body = await response.json();
   return body;
 }
-  // 이렇게 ?뒤에 keyvalue 를 지정하는 것을 query 라고 한다. 
+// 이렇게 ?뒤에 keyvalue 를 지정하는 것을 query 라고 한다. 
