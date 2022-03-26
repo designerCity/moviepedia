2105e48f93aadca685cb1615be2efce1f7014d0b
designerCity <97098829+designerCity@users.noreply.github.com>
2022-03-26 18:43:09 +0900

    Update FileInput.js

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,7 +1,9 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // 파일을 선택하여 console 에 출력해줄 함수 // props 로 name, value, onchange 를 받고,
 function FileInput({ name, value, onChange }) {
+    const [preview, setPreview] = useState();
+
     // const [value, setValue] = useState();
     const inputRef = useRef();
     const handleChange = (e)  => {
@@ -22,9 +24,18 @@ function FileInput({ name, value, onChange }) {
 
         inputNode.value = '';
         onChange(name, null)
+        setPreview()
     }
+    // 이런 식으로 component 함수에서 외부의 상태를 바꾸는 것을 sideEffect 라고 한다. 이런 sideEffect 를 할때, useEffect 를 많이 활용한다. 
+    useEffect(() => {
+        if (!value) return;
+        // objectURL 만들기 (이 함수는 문자열을 리턴한다)
+        const nextPreview = URL.createObjectURL(value)
+        setPreview(nextPreview)
+    }, [value])
     return (
         <div>
+            <img src={preview} alt="img 미리보기" height="200px"></img>
             <input type="file" onChange={handleChange} ref={inputRef} />
             {value && <button onClick={handleClearClick}>reset</button>}
         </div>
@@ -33,6 +44,4 @@ function FileInput({ name, value, onChange }) {
 // file input 은 value prop 을 지정할 수 없고, 반드시 비제어 component 로 만들어야 한다.
 // js 에서 갑을 사용자만이 바꿀 수 있게끔 만들기 위함.
 // console 창으로 보는 error 추적 : stack trace 라고 부른다.
-
-
 export default FileInput;
