b8e3c92cc3480772afff49657d577e005d7c9ec3
designerCity <97098829+designerCity@users.noreply.github.com>
2022-04-05 00:49:55 +0900

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,11 +1,34 @@
+const BASE_URL = 'https://learn.codeit.kr/api'
+
 // request 함수들을 모아놓을 파일 
 // 비동기함수 async await 사용 
 export async function getReviews({
-  order = 'createdAt', offset = 0, limit = 6,    
+  order = 'createdAt', 
+  offset = 0, 
+  limit = 6,    
 }) {
+  // throw new Error('버그가 아니라 기능입니다!')
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
-  const response = await fetch(`https://learn.codeit.kr/api/film-reviews?${query}`)
+  const response = await fetch(`${BASE_URL}/film-reviews?${query}`)
+  if (!response.ok) {
+    throw new Error('리뷰를 불러오는데 실패했습니다.')
+  }
   const body = await response.json();
   return body;
 }
-  // 이렇게 ?뒤에 keyvalue 를 지정하는 것을 query 라고 한다. 
+
+// 이렇게 ?뒤에 keyvalue 를 지정하는 것을 query 라고 한다. 
+
+export async function createReviews(formdata) {
+  const response = await fetch(
+    `${BASE_URL}/film-reviews`, {
+      method: 'POST',
+      body: formdata,
+    }
+  )
+  if (!response.ok) {
+    throw new Error('리뷰를 생성하는데 실패했습니다.')
+  }
+  const body = await response.json();
+  return body;
+}
