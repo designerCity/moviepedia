7eef2e7fd23dbae9500dd67adf0f1bb965cbe2b8
designerCity <97098829+designerCity@users.noreply.github.com>
2022-05-11 22:02:03 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://learn.codeit.kr/api'
 
-// request 함수들을 모아놓을 파일 
+// // request 함수들을 모아놓을 파일 
+
 // 비동기함수 async await 사용 
 export async function getReviews({
   order = 'createdAt', 
